a43cc21ccedaa851f7d52b7ff515cde4a06a6940
Shivam <shivamt2023@gmail.com>
2024-06-12 00:11:36 +0530

    push

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -24,25 +24,25 @@ function Footer() {
                     <h4 class="text-lg font-semibold">Popular Scriptures</h4>
                     <ul class="space-y-2">
                         <li>
-                            <a class="hover:text-indigo-500 cursor-pointer" onClick={() => navigate('/scriptures/Vedas')}>
+                            <div class="hover:text-indigo-500 cursor-pointer" onClick={() => navigate('/scriptures/Vedas')}>
                                 Vedas
-                            </a>
+                            </div>
                         </li>
                         <li>
-                            <a class="hover:text-indigo-500 cursor-pointer" onClick={() => navigate('/scriptures/Upanishad')} >
+                            <div class="hover:text-indigo-500 cursor-pointer" onClick={() => navigate('/scriptures/Upanishad')} >
                                 Upanishads
-                            </a>
+                            </div>
                         </li>
                         <li>
-                            <a class="hover:text-indigo-500 cursor-pointer" onClick={() => navigate('/scriptures/BhagvadGeeta')} >
+                            <div class="hover:text-indigo-500 cursor-pointer" onClick={() => navigate('/scriptures/BhagvadGeeta')} >
                                 Bhagavad Gita
-                            </a>
+                            </div>
                         </li>
 
                         <li>
-                            <a class="hover:text-indigo-500 cursor-pointer" onClick={() => navigate('/scriptures/Epics')}>
+                            <div class="hover:text-indigo-500 cursor-pointer" onClick={() => navigate('/scriptures/Epics')}>
                                 Epics (Ramayana, Mahabharata)
-                            </a>
+                            </div>
                         </li>
                     </ul>
                 </div>
@@ -50,31 +50,36 @@ function Footer() {
                     <h4 class="text-lg font-semibold">Important Pages</h4>
                     <ul class="space-y-2">
                         <li>
-                            <a class="hover:text-indigo-500 cursor-pointer" onClick={() => navigate('/about')}>
+                            <div class="hover:text-indigo-500 cursor-pointer" onClick={() => navigate('/about')}>
                                 About
-                            </a>
+                            </div>
                         </li>
                         <li>
-                            <a class="hover:text-indigo-500 cursor-pointer" onClick={() => navigate('')}>
+                            <div class="hover:text-indigo-500 cursor-pointer" onClick={() => navigate('')}>
                                 Contact
-                            </a>
+                            </div>
                         </li>
                         <li>
-                            <a class="hover:text-indigo-500 cursor-pointer" onClick={() => navigate('')}>
+                            <div class="hover:text-indigo-500 cursor-pointer" onClick={() => navigate('')}>
                                 Privacy Policy
-                            </a>
+                            </div>
                         </li>
                         <li>
-                            <a class="hover:text-indigo-500 cursor-pointer" onClick={() => navigate('')}>
+                            <div class="hover:text-indigo-500 cursor-pointer" onClick={() => navigate('')}>
                                 Terms of Service
-                            </a>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="hover:text-indigo-500 cursor-pointer" onClick={() => navigate('/donation')}>
+                                Donation
+                            </div>
                         </li>
                     </ul>
                 </div>
                 <div class="space-y-4">
                     <h4 class="text-lg font-semibold">Follow Us</h4>
                     <div class="flex space-x-4">
-                        <a class="text-gray-400 hover:text-indigo-500 cursor-pointer" onClick={() => navigate('')}>
+                        <div class="text-gray-400 hover:text-indigo-500 cursor-pointer" onClick={() => navigate('')}>
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="24"
@@ -89,8 +94,8 @@ function Footer() {
                             >
                                 <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
                             </svg>
-                        </a>
-                        <a class="text-gray-400 hover:text-indigo-500 cursor-pointer" onClick={() => navigate('')}>
+                        </div>
+                        <div class="text-gray-400 hover:text-indigo-500 cursor-pointer" onClick={() => navigate('')}>
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="24"
@@ -105,8 +110,8 @@ function Footer() {
                             >
                                 <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path>
                             </svg>
-                        </a>
-                        <a class="text-gray-400 hover:text-indigo-500 cursor-pointer" onClick={() => navigate('')}>
+                        </div>
+                        <div class="text-gray-400 hover:text-indigo-500 cursor-pointer" onClick={() => navigate('')}>
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="24"
@@ -123,7 +128,7 @@ function Footer() {
                                 <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
                                 <line x1="17.5" x2="17.51" y1="6.5" y2="6.5"></line>
                             </svg>
-                        </a>
+                        </div>
                     </div>
                 </div>
             </div>
